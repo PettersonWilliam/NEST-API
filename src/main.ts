cdb83792f7b626e583d0866ea2e7c90f7be7ded7
PettersonWilliam <petterson309@gmail.com>
2025-01-22 23:56:39 -0300

    feat: Add validation to recado DTOs and update main application to use global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module'; // primeiro modulo carregdo na aplicacao
+import { ValidationPipe } from '@nestjs/common'; // importando o pipe de validacao
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove campos que nao estao no DTO
+    forbidNonWhitelisted: true, // retorna um erro se tiver campos que nao estao no DTO
+    transform: true, // transforma os dados para o tipo que foi passado no DTO
+  }));
   await app.listen(process.env.PORT ?? 4000);
 }
 
--- src/recados/dto/update-recado.dto.ts
+++ src/recados/dto/update-recado.dto.ts
@@ -1,17 +1,42 @@
-export class UpdateRecadoDto {
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateRecadoDto } from "./create-recado.dto";
+import { IsBoolean, IsOptional } from "class-validator";
+
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
     // readonly -> QUANDO ADD readonly NAO PODEMOS ALTERAR O VALOR DESSA VARIAVEL
     readonly texto?: string; // Texto do recado
     readonly de?: string; // Quem enviou o recado
     readonly para?: string; // Quem recebeu o recado
 
+    @IsBoolean()
+    @IsOptional()
+    readonly lido?: boolean; // Se o recado foi lido ou não
+
     // QUANDO TBM ADD O "?" NA FRENTE DO ATRIBUTO, ELE SE TORNA OPCIONAL
     // OU SEJA PODEMOS ATULIZAR QUALQUER VALOR DO RECADO
     // NAO NECESSARIAMENTE TEMOS QUE ATUALIZAR TUDO
 
+    // USAMOS O PARTIALTYPE PARA QUE POSSAMOS ATUALIZAR APENAS OS CAMPOS QUE DESEJAMOS RECEBENDO TODA A VALIDACAO DO CREATE.DTO
+    // OU SEJA, SE TIVERMOS ALGUMA VALIDACAO NO CREATE.DTO, ELE TBM VAI TER NO UPDATE.DTO
+    // E ALEM DISSO, ELE VAI PERMITIR QUE POSSAMOS ATUALIZAR APENAS OS CAMPOS QUE DESEJAMOS
+    // E NAO TODOS OS CAMPOS
 
-    constructor(texto: string, de: string, para: string) {
-        this.texto = texto;
-        this.de = de;
-        this.para = para;
-    }
+    // EXEMPLO
+    // {
+    //     "texto": "Recado 1",
+    //     "de": "João",
+    //     "para": "Maria"
+    // }
+    // NESTE CASO, ELE VAI ATUALIZAR APENAS OS CAMPOS QUE FORAM PASSADOS
+    // E NAO TODOS OS CAMPOS
+    // E ALEM DISSO, ELE VAI VALIDAR SE OS CAMPOS QUE FORAM PASSADOS ESTAO DE ACORDO COM AS REGRAS DE VALIDACAO QUE FORAM PASSADAS NO CREATE.DTO
+    // OU SEJA, SE O TEXTO FOR MENOR QUE 3 CARACTERES, ELE VAI DAR UM ERRO
+    // E SE O DE FOR MENOR QUE 2 CARACTERES, ELE VAI DAR UM ERRO
+    // E SE O PARA FOR MENOR QUE 2 CARACTERES, ELE VAI DAR UM ERRO
+    // E SE O TEXTO FOR MAIOR QUE 255 CARACTERES, ELE VAI DAR UM ERRO
+
+    // E SE O DE FOR MAIOR QUE 50 CARACTERES, ELE VAI DAR UM ERRO
+    // E SE O PARA FOR MAIOR QUE 50 CARACTERES, ELE VAI DAR UM ERRO
+
+    // E SE O TEXTO FOR VAZIO, ELE VAI DAR UM ERRO
 }
