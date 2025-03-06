7c53d293e25251af6ef581841625b471ddc13818
PettersonWilliam <petterson309@gmail.com>
2025-03-06 16:53:47 -0300

    feat: Update pessoa and recado modules with relationships and update methods

--- src/recados/recados.module.ts
+++ src/recados/recados.module.ts
@@ -1,19 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Recado } from './entities/recado.entity';
-// import { RecadosService } from './recados.service';
-// import { RecadosController } from './recados.controller';
-// import { PessoasModule } from '../pessoas/pessoas.module'; // Importa o módulo de pessoas
-
-// @Module({
-//     imports: [TypeOrmModule.forFeature([Recado]), PessoasModule], // Importa o módulo de pessoas
-//     providers: [RecadosService],
-//     controllers: [RecadosController]
-// })
-
-// export class RecadosModule {}
-
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Recado } from './entities/recado.entity';
